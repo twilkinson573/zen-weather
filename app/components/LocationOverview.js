a88786ab3a601aa711e106566197ad8c1392f9ac
ThomasMAC <tuds_wilkinson@hotmail.com>
2016-03-30 14:24:41 +0100

    Add search by city name to LocationSearch searchbox

--- app/components/LocationOverview.js
+++ app/components/LocationOverview.js
@@ -1,11 +1,33 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+
+function puke (object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+};
+
+function LocationOverviewHeader (props) {
+  return (
+    <h3>Congrats, you searched for {props.locationString}!</h3>
+  )
+}
 
 function LocationOverview (props) {
   return (
     props.isLoading === true
-      ? <h3>Loading</h3>
-      : <h3>Congrats, you searched for {props.locationString}!</h3>
+      ? <h3>Searching for {props.locationString}...</h3>
+      : <div className='col-sm-12'>
+          <LocationOverviewHeader locationString={props.locationString}/>
+          <p>Monday is rainy</p>
+          <p>Tuesday is sunny</p>
+          <p>etc etc ...</p>
+          {puke(props.locationForecast)}
+        </div>
   )
 }
+
+LocationOverview.PropTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  locationString: PropTypes.string.isRequired
+};
 
 module.exports = LocationOverview;
--- app/containers/LocationOverviewContainer.js
+++ app/containers/LocationOverviewContainer.js
@@ -1,30 +1,36 @@
 var React = require('react');
 var Link = require('react-router').Link;
-var Axios = require('axios');
 
+var openWeatherMapHelpers = require('../utils/openweathermaphelpers');
 var ContentWrapper = require('../components/ContentWrapper');
 var LocationOverview = require('../components/LocationOverview');
-
 var LocationOverviewContainer = React.createClass({
+
   getInitialState: function () {
     return {
-      isLoading: true
+      isLoading: true,
+      locationForecast: {}
     }
   },
   componentDidMount: function () {
     // query weather API with location string
     // return & process weather data
     // pass weather data to LocationOverview as prop
-    this.setState({
-      isLoading: false
-    })
+    var searchString = this.props.location.query.locationString
+    openWeatherMapHelpers.search(searchString).then(function(searchResults) {
+      this.setState({
+        isLoading: false,
+        locationForecast: searchResults
+      })
+    }.bind(this))
   },
   render: function () {
     return (
       <ContentWrapper>
         <LocationOverview 
           isLoading={this.state.isLoading}
-          locationString={this.props.location.query.locationString} />
+          locationString={this.props.location.query.locationString}
+          locationForecast={this.state.locationForecast} />
         <Link to='/'>Search Again</Link>
       </ContentWrapper>
     )

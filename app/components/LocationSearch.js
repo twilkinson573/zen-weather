a52a112e4406b3ce11175b2ab05cae677935f99c
ThomasMAC <tuds_wilkinson@hotmail.com>
2016-03-30 14:26:27 +0100

    Add lead text to LocationSearch

--- app/components/LocationSearch.js
+++ app/components/LocationSearch.js
@@ -3,23 +3,26 @@ var PropTypes = React.PropTypes;
 
 function LocationSearch (props) {
   return (
-    <form onSubmit={props.onSubmitLocation}>
-      <div className='form-group'>
-        <input
-          className='form-control'
-          placeholder='eg. Leeds, UK'
-          onChange={props.onUpdateLocation}
-          value={props.locationString}
-          type='text'/>
-      </div>
-      <div className='form-group col-sm-4 col-sm-offset-4'>
-        <button 
-          className='btn btn-lg btn-success'
-          type='submit'>
-          Search
-        </button>
-      </div>
-    </form>
+    <div col-sm-12>
+      <h2>Enter a City & Country Code</h2>
+      <form onSubmit={props.onSubmitLocation}>
+        <div className='form-group'>
+          <input
+            className='form-control'
+            placeholder='eg. Leeds, UK'
+            onChange={props.onUpdateLocation}
+            value={props.locationString}
+            type='text'/>
+        </div>
+        <div className='form-group col-sm-4 col-sm-offset-4'>
+          <button 
+            className='btn btn-lg btn-success'
+            type='submit'>
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 

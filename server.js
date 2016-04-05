4d80aa4328f2aeaa1dbb7473795d349b9b29d45e
ThomasMAC <tuds_wilkinson@hotmail.com>
2016-04-05 11:42:22 +0100

    Production mode server working locally

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ var compression = require('compression')
 var app = express()
 
 // serve our static stuff like index.css
-app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 // send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
 var PORT = process.env.PORT || 8080
